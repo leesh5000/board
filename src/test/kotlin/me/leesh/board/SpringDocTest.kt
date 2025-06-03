0530edd6fc27524b03617336feee2072da93cc9f
leesh5000 <leesh5000@gmail.com>
2025-06-04 01:36:28 +0900

    infra: add @ActiveProfiles("test") annotation to test classes

--- src/test/kotlin/me/leesh/board/SpringDocTest.kt
+++ src/test/kotlin/me/leesh/board/SpringDocTest.kt
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 class SpringDocTest {
